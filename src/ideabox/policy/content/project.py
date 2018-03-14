12fb7c4a9840eb5cfa15548d7268a70a9ce3a7bf
Valentin Piret <valentin.piret@affinitic.be>
2018-03-14 12:39:43 +0100

    First translation and utilisation of time line

--- src/ideabox/policy/content/project.py
+++ src/ideabox/policy/content/project.py
@@ -44,3 +44,158 @@ class ProjectView(view.DefaultView):
         for content in contents:
             images_url.append(content.absolute_url)
         return images_url
+
+    def get_workflow_history(self):
+        history = self.context.workflow_history.values()[0]
+        history = list(history)
+        history.reverse()
+
+        state = history[0].get('review_state')
+        if state in ['draft', 'deposited', 'project_analysis', 'vote', 'result_analysis']:
+            draft_date = ''
+            deposited_date = ''
+            project_analysis_date = ''
+            vote_date = ''
+            result_analysis_date = ''
+            for status in history:
+                if status.get('review_state') == 'result_analysis' \
+                   and result_analysis_date == '' \
+                   and state == 'result_analysis':
+                    result_analysis_date = status.get('time')
+                if status.get('review_state') == 'vote' \
+                   and vote_date == '' \
+                   and state in ['result_analysis', 'vote']:
+                    vote_date = status.get('time')
+                if status.get('review_state') == 'project_analysis' \
+                   and project_analysis_date == '' \
+                   and state in ['result_analysis', 'vote', 'project_analysis']:
+                    project_analysis_date = status.get('time')
+                if status.get('review_state') == 'deposited' \
+                   and deposited_date == '' \
+                   and state in ['result_analysis', 'vote', 'project_analysis', 'deposited']:
+                    deposited_date = status.get('time')
+                if status.get('review_state') == 'draft' and draft_date == '':
+                    draft_date = status.get('time')
+                time_line = [draft_date, deposited_date, project_analysis_date, vote_date, result_analysis_date]
+        elif state in ['selected', 'study_in_progress', 'in_progress', 'realized']:
+            selected_date = ''
+            study_in_progress_date = ''
+            in_progress_date = ''
+            realized_date = ''
+            for status in history:
+                if status.get('review_state') == 'realized' \
+                   and realized_date == '' \
+                   and state == 'realized':
+                    realized_date = status.get('time')
+                if status.get('review_state') == 'in_progress' \
+                   and in_progress_date == '' \
+                   and state in ['realized', 'in_progress']:
+                    in_progress_date = status.get('time')
+                if status.get('review_state') == 'study_in_progress' \
+                   and study_in_progress_date == '' \
+                   and state in ['realized', 'in_progress', 'study_in_progress']:
+                    study_in_progress_date = status.get('time')
+                if status.get('review_state') == 'selected' and selected_date == '':
+                    selected_date = status.get('time')
+                time_line = [selected_date, study_in_progress_date, in_progress_date, realized_date]
+        else:
+            # Status rejected
+            draft_date = ''
+            deposited_date = ''
+            project_analysis_date = ''
+            vote_date = ''
+            result_analysis_date = ''
+            rejected_date = ''
+
+            for status in history:
+                if status.get('review_state') == 'rejected' \
+                   and rejected_date == '' \
+                   and state == 'result_analysis':
+                    rejected_date = status.get('time')
+                if status.get('review_state') == 'result_analysis' \
+                   and result_analysis_date == '' \
+                   and state in ['rejected', 'result_analysis']:
+                    result_analysis_date = status.get('time')
+                if status.get('review_state') == 'vote' \
+                   and vote_date == '' \
+                   and state in ['rejected', 'result_analysis', 'vote']:
+                    vote_date = status.get('time')
+                if status.get('review_state') == 'project_analysis' \
+                   and project_analysis_date == '' \
+                   and state in ['rejected', 'result_analysis', 'vote', 'project_analysis']:
+                    project_analysis_date = status.get('time')
+                if status.get('review_state') == 'deposited' \
+                   and deposited_date == '' \
+                   and state in ['rejected', 'result_analysis', 'vote', 'project_analysis', 'deposited']:
+                    deposited_date = status.get('time')
+                if status.get('review_state') == 'draft' and draft_date == '':
+                    draft_date = status.get('time')
+                time_line = [draft_date, deposited_date, project_analysis_date, vote_date, result_analysis_date, rejected_date]
+
+        format_time_line = []
+        for date_time in time_line:
+            if date_time != '':
+                format_time_line.append([date_time.strftime('%d'), date_time.strftime('%b')])
+
+            else:
+                format_time_line.append(['', ''])
+
+        return format_time_line
+
+    def get_time_line_title(self):
+        history = self.context.workflow_history.values()[0]
+        history = list(history)
+        history.reverse()
+        state = history[0].get('review_state')
+
+        if state in ['draft', 'deposited', 'project_analysis', 'vote', 'result_analysis']:
+            time_line_title = [_(u'draft'),
+                               _(u'deposited'),
+                               _(u'project analysis'),
+                               _(u'vote'),
+                               _(u'result analysis')
+                               ]
+        elif state in ['selected', 'study_in_progress', 'in_progress', 'realized']:
+            time_line_title = [_(u'selected'),
+                               _(u'study in progress'),
+                               _(u'in progress'),
+                               _(u'realized')
+                               ]
+        else:
+            time_line_title = [_(u'draft'),
+                               _(u'deposited'),
+                               _(u'project analysis'),
+                               _(u'vote'),
+                               _(u'result analysis'),
+                               _(u'rejected')
+                               ]
+        return time_line_title
+
+    def get_current_status(self):
+        history = self.context.workflow_history.values()[0]
+        history = list(history)
+        history.reverse()
+
+        state = history[0].get('review_state')
+        if state == 'draft':
+            state = _(u'draft')
+        if state == 'deposited':
+            state = _(u'deposited')
+        if state == 'project_analysis':
+            state = _(u'project analysis')
+        if state == 'vote':
+            state = _(u'vote')
+        if state == 'result_analysis':
+            state = _(u'result analysis')
+        if state == 'rejected':
+            state = _(u'rejected')
+        if state == 'selected':
+            state = _(u'selected')
+        if state == 'study_in_progress':
+            state = _(u'study in progress')
+        if state == 'in_progress':
+            state = _(u'in progress')
+        if state == 'realized':
+            state = _(u'realized')
+
+        return state

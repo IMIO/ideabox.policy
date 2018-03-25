28ec14fe4460fb8457919889e76e40597451aa5f
Martin Peeters <martin.peeters@affinitic.be>
2018-03-25 18:21:55 +0200

    Randomize the order of items in projects faceted view

---
+++ src/ideabox/policy/faceted/projects.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from Products.Five import BrowserView
+from datetime import datetime
+from hashlib import md5
+from operator import attrgetter
+from random import shuffle
+
+
+class ProjectsView(BrowserView):
+
+    _keys = {
+        0: 'UID',
+        1: 'Title',
+        2: 'created',
+        3: 'modified',
+    }
+
+    @property
+    def random_sort_key(self):
+        key = ord(self.random_key[:1])
+        return self._keys[key % len(self._keys)]
+
+    def random_sort(self, elements):
+        elements._sequence = sorted(
+            elements._sequence,
+            key=attrgetter(self.random_sort_key),
+        )
+        return elements
+
+    def sort_items(self, elements):
+        elements = [e for e in elements]
+        shuffle(elements)
+        return elements
+
+    @property
+    def random_key(self):
+        now = datetime.now()
+        return md5(u'{0}-{1}'.format(
+            self.request.get('HTTP_USER_AGENT', now.strftime('%m%Y')),
+            now.strftime('%d'),
+        )).hexdigest()

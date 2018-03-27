481df5437e8edb378cd94d5e8f81ed0b08fe54e5
Martin Peeters <martin.peeters@affinitic.be>
2018-03-27 11:18:36 +0200

    Add a vocabulary for project states

--- src/ideabox/policy/vocabularies.py
+++ src/ideabox/policy/vocabularies.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 from zope.schema.vocabulary import SimpleVocabulary
+from Products.CMFPlone import PloneMessageFactory as PMF
 
 from ideabox.policy import _
 
@@ -29,9 +30,30 @@ class ThemeVocabularyFactory(object):
                   {'INSO': _('INSO', u'Inclusion sociale')},
                   {'MOBI': _('MOBI', u'La mobilité')},
                   {'TREN': _('TREN', u'La transition énergétique')},
-                  {'VEAU': _('VEAU', u'La végétalisation et l\'agriculture urbaine')},
+                  {'VEAU': _('VEAU', u"La végétalisation et l'agriculture urbaine")},
                   {'VPCN': _('VPCN', u'Ville participative, Collaborative et Numérique')}]
         return dict_list_2_vocabulary(values)
 
 
 ThemeVocabulary = ThemeVocabularyFactory()
+
+
+class ReviewStateVocabularyFactory(object):
+
+    def __call__(self, context):
+        values = [
+            {'draft': PMF('draft')},
+            {'deposited': PMF('deposited')},
+            {'projectPMFanalysis': PMF('projectPMFanalysis')},
+            {'vote': PMF('vote')},
+            {'resultPMFanalysis': PMF('resultPMFanalysis')},
+            {'selected': PMF('selected')},
+            {'rejected': PMF('rejected')},
+            {'studyPMFinPMFprogress': PMF('studyPMFinPMFprogress')},
+            {'inPMFprogress': PMF('inPMFprogress')},
+            {'realized': PMF('realized')},
+        ]
+        return dict_list_2_vocabulary(values)
+
+
+ReviewStateVocabulary = ReviewStateVocabularyFactory()

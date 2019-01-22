fbf8cd5597e9d44d997f7cd16f0b0f893ef8cc27
Piret Valentin <valentin.piret@affinitic.be>
2019-01-22 15:15:53 +0100

    Add action on send button for project_submission

--- src/ideabox/policy/form/project_submission.py
+++ src/ideabox/policy/form/project_submission.py
@@ -4,12 +4,19 @@ import zope.interface
 
 from ideabox.policy import _
 from ideabox.policy.content.project import IProject
+from ideabox.policy.utils import execute_under_admin
+from plone import api
 from plone.namedfile.field import NamedBlobImage
 from z3c.form import button
 from z3c.form.interfaces import HIDDEN_MODE
+from zope import schema
 
 
 class IProjectSubmission(IProject):
+    title = schema.TextLine(
+        title=_(u'Title'),
+        required=True,
+    )
 
     project_image = NamedBlobImage(
         title=_(u'Project image'),
@@ -20,15 +27,50 @@ class IProjectSubmission(IProject):
 class ProjectSubmissionForm(z3c.form.form.Form):
 
     zope.interface.implements(z3c.form.interfaces.IFieldsForm)
-    fields = z3c.form.field.Fields(IProjectSubmission)
+    fields = z3c.form.field.Fields(IProjectSubmission).select(
+        'title',
+        'project_theme',
+        'body',
+        'project_image',
+        'original_author',
+    )
 
     fields['original_author'].mode = HIDDEN_MODE
 
     ignoreContext = True
 
-    def send_request(self):
-        """TO DO"""
+    def update(self):
+        super(ProjectSubmissionForm, self).update()
+        self.widgets['original_author'].value = \
+            api.user.get_current().getProperty('fullname')
+
+    def send_request(self, data):
+        container = api.portal.get().projets
+
+        project_obj = execute_under_admin(
+            container,
+            api.content.create,
+            type='Project',
+            title=data['title'],
+            project_theme=data['project_theme'],
+            body=data['body'].output,
+            container=container,
+            original_author=data['original_author'])
+        if data['project_image']:
+            execute_under_admin(
+                project_obj,
+                api.content.create,
+                type='Image',
+                title=data['project_image'].filename,
+                image=data['project_image'],
+                container=project_obj
+            )
 
     @button.buttonAndHandler(_(u'Send'), name='send')
     def handleApply(self, action):
-        self.send_request()
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+
+        self.send_request(data)
--- src/ideabox/policy/utils.py
+++ src/ideabox/policy/utils.py
@@ -1,8 +1,11 @@
 # encoding: utf-8
 
-from zope.i18n import translate
-
+from AccessControl import getSecurityManager
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SecurityManagement import setSecurityManager
+from AccessControl.User import UnrestrictedUser as BaseUnrestrictedUser
 from ideabox.policy import vocabularies
+from zope.i18n import translate
 
 
 def token_type_recovery(value):
@@ -10,3 +13,35 @@ def token_type_recovery(value):
     vocabulary = vocabularies.ThemeVocabulary(None)
     return [e.token for e in vocabulary.by_value.values()
             if translate(e.title) == value][0]
+
+
+class UnrestrictedUser(BaseUnrestrictedUser):
+    """Unrestricted user that still has an id.
+    """
+
+    def getId(self):
+        """Return the ID of the user.
+        """
+        return self.getUserName()
+
+
+def execute_under_admin(portal, function, *args, **kwargs):
+    """ Execude code under admin privileges """
+    sm = getSecurityManager()
+    try:
+        try:
+            tmp_user = UnrestrictedUser(
+                'admin', '', [''], ''
+            )
+            # Wrap the user in the acquisition context of the portal
+            tmp_user = tmp_user.__of__(portal.acl_users)
+            newSecurityManager(None, tmp_user)
+            # Call the function
+            return function(*args, **kwargs)
+
+        except:
+            # If special exception handlers are needed, run them here
+            raise
+    finally:
+        # Restore the old security manager
+        setSecurityManager(sm)

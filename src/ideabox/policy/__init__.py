c22a3d1d9f6364d4bc54164c199b44b9c99b4ee5
Nicolas Demonte <nicolas@affinitic.be>
2019-01-25 13:59:09 +0100

    Removal of cpskin dependencies for Plone 5 transition

--- src/ideabox/policy/__init__.py
+++ src/ideabox/policy/__init__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from zope.i18nmessageid import MessageFactory
-from cpskin.core.browser import folderview
 
 
 _ = MessageFactory('ideabox.policy')
-
-folderview.ADDABLE_TYPES += ['Link']

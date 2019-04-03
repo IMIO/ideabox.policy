5ed10e0ee888ae331cc1634ebaf1e2dd134de507
Martin Peeters <martin.peeters@affinitic.be>
2019-04-03 21:18:57 +0200

    Back to development: 2.0b7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="ideabox.policy",
-    version="2.0b6",
+    version="2.0b7.dev0",
     description="IdeaBox",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

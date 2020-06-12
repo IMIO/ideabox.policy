a64e2149f80a45c97fc324475a4de406e142c0f1
Martin Peeters <martin.peeters@affinitic.be>
2020-06-12 16:41:49 +0200

    Back to development: 3.1.0a5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="ideabox.policy",
-    version="3.1.0a4",
+    version="3.1.0a5.dev0",
     description="IdeaBox",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

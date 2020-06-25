7ad4ea20fd977258ac632455e1a38cafc3759095
Martin Peeters <martin.peeters@affinitic.be>
2020-06-25 19:11:22 +0200

    Add an upgrade step for priority action tiles

--- src/ideabox/policy/upgrades.py
+++ src/ideabox/policy/upgrades.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from plone import api
+from plone.registry import field
+from plone.registry import Record
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
 
 
 def fix_comments_1001(context):
@@ -40,5 +45,69 @@ def to_1003(context):
 
 
 def to_1004(context):
+    """ Add priority action, progress and campaign content types """
     portal_setup = api.portal.get_tool("portal_setup")
     portal_setup.runImportStepFromProfile("profile-ideabox.policy:default", "typeinfo")
+
+
+def to_1005(context):
+    """ Add new tiles for priority action """
+    registry = getUtility(IRegistry)
+    records = registry.records
+
+    tiles = {
+        "plone.app.mosaic.app_tiles.priorityactions": {
+            "name": "ideabox.policy.priority_actions",
+            "label": "Latest priority actions",
+            "category": "advanced",
+            "tile_type": "app",
+            "default_value": "",
+            "read_only": False,
+            "settings": True,
+            "favorite": False,
+            "rich_text": False,
+            "weight": 10,
+        },
+        "plone.app.mosaic.app_tiles.random_priorityactions": {
+            "name": "ideabox.policy.random_priority_actions",
+            "label": "Random priority actions",
+            "category": "advanced",
+            "tile_type": "app",
+            "default_value": "",
+            "read_only": False,
+            "settings": True,
+            "favorite": False,
+            "rich_text": False,
+            "weight": 10,
+        },
+    }
+    types = {
+        "name": (field.TextLine, "Name"),
+        "label": (field.TextLine, "Label"),
+        "category": (field.TextLine, "Category"),
+        "tile_type": (field.TextLine, "Type"),
+        "default_value": (field.TextLine, "Default value"),
+        "read_only": (field.Bool, "Read only"),
+        "settings": (field.Bool, "Settings"),
+        "favorite": (field.Bool, "Favorite"),
+        "rich_text": (field.Bool, "Rich Text"),
+        "weight": (field.Int, "Weight"),
+    }
+
+    for name, values in tiles.items():
+        for key, value in values.items():
+            r_key = "{0}.{1}".format(name, key)
+            field_type, title = types[key]
+            required = True
+            if key == "default_value":
+                required = False
+            if r_key in records:
+                continue
+            record = Record(
+                field_type(
+                    title=title,
+                    required=required
+                ),
+                value=value
+            )
+            records[r_key] = record

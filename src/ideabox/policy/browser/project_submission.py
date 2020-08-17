6ff435f11bd35ee5d27468018dd3056008c9fbb3
Christophe Boulanger <christophe.boulanger@imio.be>
2020-08-17 09:53:47 +0200

    enable_submission don't look about a registry value anymore but look at the context (campaign) field value.

--- src/ideabox/policy/browser/project_submission.py
+++ src/ideabox/policy/browser/project_submission.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from ideabox.policy.form.project_submission import ProjectSubmissionForm
-from plone import api
 from plone.z3cform.layout import FormWrapper
 
 
@@ -9,7 +8,5 @@ class ProjectSubmissionView(FormWrapper):
     form = ProjectSubmissionForm
 
     def enable_submission(self):
-        return api.portal.get_registry_record(
-            "ideabox.policy.browser.controlpanel.IIdeaBoxSettingsSchema.project_submission",
-            default=True,
-        )
+        context = self.context
+        return context.project_submission

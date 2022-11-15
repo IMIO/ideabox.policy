5a15758a51ed0b5c65dd9a8955fbdc830255a0cf
Christophe Boulanger <christophe.boulanger@imio.be>
2022-11-15 08:34:23 +0100

    Back to development: 3.4.16

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="ideabox.policy",
-    version="3.4.15",
+    version="3.4.16.dev0",
     description="IdeaBox",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
